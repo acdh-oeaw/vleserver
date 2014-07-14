60148b83f42ace300050e45c6939df5e9f9e0cf9
Omar Siam <Omar.Siam@oeaw.ac.at>
2014-07-14 18:18:48 +0200

    Add code to create the dictionary tables and auxilliary tables, finish rights management, enable changing of passwords and rights for admin users.

--- module/wde/src/wde/V2/Rest/Users/UsersResource.php
+++ module/wde/src/wde/V2/Rest/Users/UsersResource.php
@@ -46,12 +46,15 @@ class UsersResource extends DbConnectedResource {
     public function fetchAll($data = array()) {
         $this->tableName = $this->event->getRouteParam('dict_name');
         $this->tablesWithAuth = array_keys($this->getIdentity()->getAuthenticationIdentity());
-        if (!(in_array($this->tableName, $this->tablesWithAuth) || $this->isAdmin)) {
+        if (!(in_array($this->tableName, $this->tablesWithAuth) || $this->isAdmin())) {
             return new ApiProblem(403, 'Not allowed. You are not authorized for this dictionary.');
         }
         $filter = new Where();
         if (!$this->isAdmin()) {
             $filter->equalTo('userID', $this->identity->getName());
+        }
+        if($this->tableName !== 'dict_users') {
+            $filter->equalTo('table', $this->tableName);
         }
         $adapter = new DbTableGateway($this->table, $filter);
         return new $this->collectionClass($adapter);
@@ -72,39 +75,70 @@ class UsersResource extends DbConnectedResource {
     
     public function create($data) {
         $this->tableName = $this->event->getRouteParam('dict_name');
-        if (($this->tableName !== 'dict_users') && ($this->tableName !== $data->table)) {
-            return new ApiProblem(403, 'You may only set user rights for any table using dict_users.');
-        }
+        $userIdFromPath = $this->event->getRouteParam('users_id');
         $this->tablesWithAuth = array_keys($this->getIdentity()->getAuthenticationIdentity());
-        try {
-            $anyUsers = $this->table->select();
-        }
-        catch (\Exception $e)
-        {
-            return new ApiProblem(404, 'Database is not initialized.');
-        }
-        if ($anyUsers->count() === 0) {
-            // Initial setup case
-            if ($data->table !== 'dict_users' ||
-                $data->userId !== $this->getIdentity()->getName()) {
-                return new ApiProblem(403, 'You need to register yourself as admin user first.');
+        if (null === $userIdFromPath) {
+            /* POST to collection */
+            if (($this->tableName !== 'dict_users') && ($this->tableName !== $data->table)) {
+                return new ApiProblem(403, 'You may only set user rights for any table using dict_users.');
             }
+            try {
+                $anyUsers = $this->table->select();
+            } catch (\Exception $e) {
+                return new ApiProblem(404, 'Database is not initialized.');
+            }
+            if ($anyUsers->count() === 0) {
+                // Initial setup case
+                if ($data->table !== 'dict_users' ||
+                        $data->userId !== $this->getIdentity()->getName()) {
+                    return new ApiProblem(403, 'You need to register yourself as admin user first.');
+                }
+            } else {
+                if (!$this->isAdmin()) {
+                    return new ApiProblem(403, 'You need to be an admin user to do this.');
+                }
+                if ($data->table === 'dict_users' && in_array('dict_users', $this->tablesWithAuth)) {
+                    return new ApiProblem(403, 'You are already an admin user.');
+                }
+                $existsCheck = $this->table->select(array(
+                    'userId' => $data->userId,
+                    'table' => $data->table,
+                ));
+                if ($existsCheck->count() > 0) {
+                    return new ApiProblem(403, 'The user already has access rights to this table.');
+                }
+            }
+            return parent::create($data);
         } else {
-            if (!$this->isAdmin()) {
-                return new ApiProblem(403, 'You need to be an admin user to do this.');
+            /* POST to entity */
+            if (isset($data->id) && ($data->id !== $userIdFromPath)) {
+                return new ApiProblem(403, 'Changing the id is not allowed.');
             }
-            if ($data->table === 'dict_users' && in_array('dict_users', $this->tablesWithAuth)) {
-                return new ApiProblem(403, 'You are already an admin user.');
+            $storedUser = $this->fetch($userIdFromPath);
+            if ($storedUser instanceof ApiProblem) {
+                return $storedUser;
             }
-            $existsCheck = $this->table->select(array(
-                'userId' => $data->userId,
-                'table' => $data->table,
-            ));
-            if ($existsCheck->count() > 0) {
-                return new ApiProblem(403, 'The user already has access rights to this table.');
+            $data->id = $userIdFromPath;
+            $inputFilter = $this->getEvent()->getInputFilter();
+            if (null !== $inputFilter) {
+                $inputFilter->setData((array) $data);
             }
+            if ($this->isAdmin()) {
+                if ($data->userId !== $storedUser['userID'] ||
+                    $data->table !== $storedUser['table']) {
+                    return new ApiProblem(403, 'Only changing rights and passord is allowed.');
+                }
+            } else {
+                 if ($data->userId !== $storedUser['userID'] ||
+                        $data->read !== $storedUser['read'] ||
+                        $data->write !== $storedUser['write'] ||
+                        $data->writeown !== $storedUser['writeown'] ||
+                        $data->table !== $storedUser['table']) {
+                    return new ApiProblem(403, 'Unprivileged users may only change their password.');
+                }               
+            }           
+            return parent::patch($userIdFromPath, $data);
         }
-        return parent::create($data);
     }
 }
 

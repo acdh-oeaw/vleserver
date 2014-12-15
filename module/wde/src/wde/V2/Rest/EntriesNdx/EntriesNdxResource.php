5dad6c5352fa7352e5100a5e6f7d2a5dd217082d
Omar Siam <simar@gmx.net>
2014-12-15 21:02:14 +0100

    Implement fetchall filtereg by the id in the path, try implement create.

--- module/wde/src/wde/V2/Rest/EntriesNdx/EntriesNdxResource.php
+++ module/wde/src/wde/V2/Rest/EntriesNdx/EntriesNdxResource.php
@@ -4,11 +4,25 @@ namespace wde\V2\Rest\EntriesNdx;
 
 use wde\V2\Rest\Common\AccessCheckingTSResource;
 use Zend\Db\TableGateway\TableGatewayInterface as TableGateway;
+use wde\V2\Rest\Common\LimitedColumnTableGateway;
+use Zend\Db\Sql\Where;
 
 class EntriesNdxResource extends AccessCheckingTSResource {
     
     public function __construct(TableGateway $table, $identifierName, $collectionClass) {
         $this->realTableNameExtension = '_ndx';
         parent::__construct($table, $identifierName, $collectionClass);
+    }
+    
+    public function fetchAll($data = array()) {        
+        parent::fetchAll($data);
+        $entryFilter = new Where();
+        $entryFilter->equalTo('id', $this->getEvent()->getRouteParam('entries_id'));
+        $adapter = new LimitedColumnTableGateway($this->table, array(), $entryFilter, array());
+        return new EntriesNdxCollection($adapter);
+    }
+    
+    public function create($data) {
+        return parent::create($data);
     }
 }
